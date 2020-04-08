dee7fee30211e17bd42b2f417cf1bceedcc128bf
Paweł Wieczorek <contact@cerberos.pl>
2020-04-08 20:56:22 +0200

    Add documentation for the rest of files in expression package

--- src/expression/ExpressionErrorHandler.java
+++ src/expression/ExpressionErrorHandler.java
@@ -1,5 +1,8 @@
 package expression;
 
+/**
+ * Interface specifying format of handler called in case of expression parsing error
+ */
 @FunctionalInterface
 public interface ExpressionErrorHandler {
     void handle(String header, String content);
--- src/expression/IncorrectExpressionException.java
+++ src/expression/IncorrectExpressionException.java
@@ -1,5 +1,8 @@
 package expression;
 
+/**
+ * Exception thrown by JShellApi in case of error during expression execution
+ */
 public class IncorrectExpressionException extends Exception {
     public IncorrectExpressionException(String e) {
         super(e);
