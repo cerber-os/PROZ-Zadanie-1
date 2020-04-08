f8de5854ad4fb6dffbf7578e0e412eab48633158
Paweł Wieczorek <contact@cerberos.pl>
2020-04-08 22:12:09 +0200

    Refactor Expression.java file and add JavaDoc compatible comments

--- src/expression/Expression.java
+++ src/expression/Expression.java
@@ -1,5 +1,8 @@
 package expression;
 
+/**
+ * Class used for generating arithmetical expression based on user input
+ */
 public class Expression {
     private final ExpressionErrorHandler handler;
     private String firstArgument = "";
@@ -11,80 +14,118 @@ public class Expression {
         handler = h;
     }
 
+    /**
+     * Evaluate expresion entered by user
+     */
     public void compute() {
         try {
             firstArgument = JShellApi.executeTwoArgsExpr(firstArgument, operator, secondArgument);
             operator = secondArgument = "";
-        } catch(IncorrectExpressionException e) {
+        } catch (IncorrectExpressionException e) {
             handler.handle("Expression contains error", e.toString());
             clear();
         }
     }
 
+    /**
+     * Check whether result field stores user input or expression value; in case the second, clear result field on digit press
+     * <br> This method is used to decide whether the result field should be cleared after user action
+     * @param clearIfExists pressed key should cause result field to be cleared
+     */
     private void checkResult(boolean clearIfExists) {
-        if(clearOnDigit) {
+        if (clearOnDigit) {
             clearOnDigit = false;
-            if(clearIfExists)
+            if (clearIfExists)
                 firstArgument = "";
         }
     }
 
+    /**
+     * Function called when user expects the result of provided expression
+     * @return the value of computed expression
+     */
     public String equals() {
         compute();
         clearOnDigit = true;
         return firstArgument;
     }
 
+    /**
+     * Reset state of calculator
+     * @return value to be shown after resetting calculator
+     */
     public String clear() {
         firstArgument = secondArgument = operator = "";
         return "0";
     }
 
+    /**
+     * Handle new digit
+     * @param digit number pressed by user
+     * @return the new value of result box
+     */
     public String digit(String digit) {
+        // Clear result box if it contains result of the previous expression
         checkResult(true);
-        if(operator.equals("")) {
-            if(firstArgument.equals("") && digit.equals("0"))
+        if (operator.equals("")) {
+            if (firstArgument.equals("") && digit.equals("0"))
                 return "0";
             else
                 return firstArgument += digit;
         } else {
-            if(secondArgument.equals("") && digit.equals("0"))
+            if (secondArgument.equals("") && digit.equals("0"))
                 return "0";
             else
                 return secondArgument += digit;
         }
     }
 
+    /**
+     * Handle operator
+     * <br> if operator was already selected, second press caused the previous operations to be computed
+     * @param op operator pressed by user
+     * @return the new value of result box
+     */
     public String operator(String op) {
         checkResult(false);
         if (!secondArgument.equals("")) {
             compute();
         }
         operator = op;
-        if(firstArgument.equals(""))
+        if (firstArgument.equals(""))
             firstArgument = "0";
         return firstArgument;
     }
 
+    /**
+     * Handle comma key
+     * <br> Alse, prevent user from entering more than one comma in number
+     * @return the new value of result box
+     */
     public String comma() {
         checkResult(true);
-        if(operator.equals("")) {
-            if(firstArgument.equals(""))
+        if (operator.equals("")) {
+            if (firstArgument.equals(""))
                 return firstArgument = "0.";
-            else if(firstArgument.contains("."))
+            else if (firstArgument.contains("."))
                 return firstArgument;
             else
                 return firstArgument += ".";
         } else {
-            if(secondArgument.equals(""))
+            if (secondArgument.equals(""))
                 return secondArgument = "0.";
-            else if(secondArgument.contains("."))
+            else if (secondArgument.contains("."))
                 return secondArgument;
             else
                 return secondArgument += ".";
         }
     }
 
+    /**
+     * Handle extra functions (like: sqrt)
+     * @param funcName id of function to be called
+     * @return the new value of result box
+     */
     public String exFunc(String funcName) {
         checkResult(false);
         if (!secondArgument.equals("")) {
@@ -94,7 +135,7 @@ public class Expression {
             firstArgument = JShellApi.executeOneArgExpr(firstArgument, funcName);
             operator = secondArgument = "";
             clearOnDigit = true;
-        } catch(IncorrectExpressionException e) {
+        } catch (IncorrectExpressionException e) {
             handler.handle("Expression contains error", e.toString());
             clear();
             return "0";
