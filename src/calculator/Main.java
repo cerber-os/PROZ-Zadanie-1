48d1462dec6caa081250e24b386cec87c95d41c5
Paweł Wieczorek <contact@cerberos.pl>
2020-04-08 19:42:26 +0200

    Add JavaDocs comments for Main.java file

--- src/calculator/Main.java
+++ src/calculator/Main.java
@@ -6,10 +6,28 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+/**
+ * Application main class
+ */
 public class Main extends Application {
 
+    /**
+     * Program entry point
+     * @param args Arguments passed by command line
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    /**
+     * Functions called by JavaFX framework at the start of application.
+     * <br> Load scene from calculator.fxml file, set basic properties and show window.
+     *
+     * @param primaryStage
+     * @throws Exception
+     */
     @Override
-    public void start(Stage primaryStage) throws Exception{
+    public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("calculator.fxml"));
         primaryStage.setScene(new Scene(root));
         primaryStage.setTitle("Calculator");
@@ -17,10 +35,5 @@ public class Main extends Application {
         primaryStage.setMinWidth(320);
         primaryStage.centerOnScreen();
         primaryStage.show();
-    }
-
-
-    public static void main(String[] args) {
-        launch(args);
     }
 }
